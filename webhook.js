b1448dc25ffbb4b37f2ad605a16927eb0aa051a3
Turkyden <616287091@qq.com>
2019-07-27 14:55:39 +0800

    Added the webhook

--- webhook.js
+++ webhook.js
@@ -1,15 +1,34 @@
-// this is webhook script
+const http = require('http');
+const crypto = require('crypto');
+const exec = require('child_process').exec;
 
+const secret = "wued2019";
+//const repo = "C://github/wued-dreamer";
 
-// maksome nodes tete
+//打开 8080 端口监听来自 Github 的 Push 推送
+http.createServer(function (req, res) {
+  let chunks = ''
+  req.on('data', function(chunk) {
+	  chunks += chunk.toString();
+      const sig = "sha1=" + crypto.createHmac('sha1', secret).update(chunks).digest('hex');
+      if (req.headers['x-hub-signature'] == sig) {
+        exec('git pull');
+        //exec('cd ' + repo + ' && git pull');
+      }
+  });
 
+  res.end('Hello World To Github\n');
+}).listen(8080);
 
-// ytryry
+// const http = require('http');
+// const hostname = '0.0.0.0';
+// const port = 8080;
+// const server = http.createServer((req, res) => { 
+    // res.statusCode = 200;
+    // res.setHeader('Content-Type', 'text/plain');
+    // res.end('Hello World\n');
+// }); 
 
-// fdfdf  vvc cv
-
-
-yggyg 98809898 c c c c
-
-
-gfdgfd
+// server.listen(port, hostname, () => { 
+    // console.log(`Server running at http://${hostname}:${port}/`);
+// });
