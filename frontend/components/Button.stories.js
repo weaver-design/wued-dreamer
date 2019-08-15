d868edc3095bcac1b1e5ca2ed0a56f9f54a38559
wj871287@gmail.com <wj871287@gmail.com>
2019-08-15 15:02:53 +0800

    Added the storybook

---
+++ frontend/components/Button.stories.js
@@ -0,0 +1,13 @@
+import Vue from 'vue';
+import { storiesOf } from '@storybook/vue';
+import MyButton from './Button.vue';
+
+Vue.component('my-button', MyButton);
+
+storiesOf('Button', module)
+  .add('with text', () => '<my-button>with text</my-button>')
+  .add('with emoji', () => '<my-button>😀 😎 👍 💯</my-button>')
+  .add('as a component', () => ({
+    components: { MyButton },
+    template: '<my-button :rounded="true">rounded</my-button>'
+  }));
